b311030d1c29accd9d1def4f8db74903aaa7bb18
Anastassia Shaitarova <anastassia.shaitarova@uzh.ch>
2025-10-13 09:12:07 +0200

    added protoptype version of functions to get saccades and map fixations to aoi

--- peyeplineAS.py
+++ peyeplineAS.py
@@ -1,10 +1,8 @@
 import pymovements as pm
 from pathlib import Path
-from pymovements.gaze import Gaze
 from pymovements.gaze.io import from_asc
 import polars as pl
-
-# from pymovements.gaze.Gaze import compute_event_properties
+from pymovements.stimulus.text import from_file
 
 
 def create_gaze_frame(raw_file: str, save=True) -> pm.Gaze:
@@ -35,14 +33,40 @@ def create_gaze_frame(raw_file: str, save=True) -> pm.Gaze:
     # print the metadata
     print(gaze.experiment)
 
+    # ### Gaze.save() ####
+
+    # def save(
+    #     self,
+    #     dirpath: str | Path,
+    #     *,
+    #     save_events: bool | None = None,
+    #     save_samples: bool | None = None,
+    #     save_experiment: bool | None = None,
+    #     verbose: int = 1,
+    #     extension: str = 'feather',
+    # ) -> Gaze:
+    #     """Save data from the Gaze object in the provided directory.
+
+    #     Depending on parameters it may save three files:
+    #     * preprocessed gaze in samples (samples)
+    #     * calculated gaze events (events)
+    #     * metadatata experiment in YAML file (experiment).
+
+    #     Data will be saved as feather or csv files.
+
+    #     Verbosity level (0: no print output, 1: show progress bar, 2: print saved filepaths)
+
     if save:
-        # save gaze data as pickle file in the working folder
-        gaze.save(Path(raw_file).with_suffix(".pkl"), save_experiment=True)
+        gaze.save(Path(raw_file).parent, save_events=True,
+                  save_samples=True, save_experiment=True, extension='feather')
+        # more human-readable csv format
+        gaze.save(Path(raw_file).parent, save_events=True,
+                  save_samples=True, save_experiment=True, extension='csv', verbose=1)
+
+        # there is no built-in pickle save function in pymovements
+        # gaze.save(Path(raw_file).with_suffix(".pkl"), save_experiment=True)
+
     return gaze
-
-
-gaze = create_gaze_frame(
-    "data/MultiplEYE_SQ_CH_Zurich_1_2025/eye-tracking-sessions/006_SQ_CH_1_ET1/006sqch1.asc", save=True)
 
 
 def calculate_fixations(gaze: pm.Gaze):
@@ -56,26 +80,111 @@ def calculate_fixations(gaze: pm.Gaze):
     print(gaze.samples.head())
 
     gaze.pix2deg()
+    # Compute gaze positions in degrees of visual angle from pixel position coordinates.
+    # This method requires a properly initialized :py:attr:`~.Gaze.experiment` attribute.
+    # After success, :py:attr:`~.Gaze.samples` is extended by the resulting dva position columns.
+
     gaze.pos2vel('smooth')
+    # Compute gaze velocity in dva/s from dva position coordinates.
+    # This method requires a properly initialized :py:attr:`~.Gaze.experiment` attribute.
+    # After success, :py:attr:`~.Gaze.samples` is extended by the resulting velocity columns.
+
+    # Parameters
+    # ----------
+    # method: str
+    #     Computation method. See :func:`~transforms.pos2vel()` for details, default: fivepoint.
+    #     (default: 'fivepoint')
+    # **kwargs: int | float | str
+    #     Additional keyword arguments to be passed to the :func:`~transforms.pos2vel()` method.
 
     print('Newly computed fixations:')
     print()
+
     # recompute events (e.g. with IVT), produces fixations
-    gaze.detect(method="ivt", velocity_threshold=30)
-    fixations_ivt = gaze.events.frame.filter(pl.col("name") == "fixation")
+    gaze.detect(method="ivt", velocity_threshold=30, name="fixation.ivt")
+
+    # Detect events by applying a specific event detection method.
+
+    # Parameters
+    # ----------
+    # method: Callable[..., pm.Events] | str
+    #     The event detection method to be applied.
+    # eye: str
+    #     Select which eye to choose. Valid options are ``auto``, ``left``, ``right`` or ``None``.
+    #     If ``auto`` is passed, eye is inferred in the order ``['right', 'left', 'eye']`` from
+    #     the available columns in :py:attr:`~.Gaze.samples`. (default: 'auto')
+    # clear: bool
+    #     If ``True``, event DataFrame will be overwritten with new DataFrame instead of being
+    #     merged into the existing one. (default: False)
+    # **kwargs: Any
+    #     Additional keyword arguments to be passed to the event detection method.
+
+    fixations_ivt = gaze.events.frame.filter(pl.col("name") == "fixation.ivt")
     print(fixations_ivt.head())
 
-
-calculate_fixations(gaze)
+    return gaze
 
 
 def calculate_saccades(gaze: pm.Gaze):
-    pass
+
+    print()
+    print('Print gaze.events.frame before microsaccade detection:')
+    print(gaze.events.frame)
+    gaze.detect('microsaccades', minimum_duration=12)
+    print(gaze.events.frame.filter(pl.col("name") == "microsaccades").head())
+
+    return gaze
 
 
-def map_fixations_to_aois():
+def map_fixations_to_aois(gaze: pm.Gaze):
     """
     The aoi files are stored in the data collection. They are the same for each participant. Excpet for the question answer options.
     :return:
     """
-    pass
+
+    aoi_chars_file = "data/MultiplEYE_SQ_CH_Zurich_1_2025/eye-tracking-sessions/data_piloting_stimuli_MultiplEYE_SQ_CH_Zurich_1_2025participant_id_1_to_5/aoi_stimuli_sq_ch_1/lit_magicmountain_6_aoi.csv"
+
+    stimulus = from_file(
+        aoi_path=aoi_chars_file,
+        aoi_column="char",
+        start_x_column="top_left_x",
+        start_y_column="top_left_y",
+        width_column="width",
+        height_column="height",
+        page_column="page",
+    )
+
+    print(stimulus.aois.head(10))
+
+    # row = {"x": 160.0, "y": 95.0}  # somewhere over the word "Magjik"
+    # aoi = stimulus.get_aoi(row=row, x_eye="x", y_eye="y")
+    # print(aoi)
+
+    #  We map each gaze point to an aoi, considering the boundary still part of the area of interest.
+
+    # Parameters
+    # ----------
+    # aoi_dataframe: pm.stimulus.TextStimulus
+    #     Area of interest dataframe.
+    # eye: str
+    #     String specificer for inferring eye components. Supported values are: auto, mono, left
+    #     right, cyclops. Default: auto.
+    # gaze_type: str
+    #     String specificer for whether to use position or pixel coordinates for
+    #     mapping. Default: pixel.
+
+    gaze.map_to_aois(
+        aoi_dataframe=stimulus,
+        eye="auto",
+        gaze_type="pixel"
+    )
+
+
+gaze = create_gaze_frame(
+    "data/MultiplEYE_SQ_CH_Zurich_1_2025/eye-tracking-sessions/006_SQ_CH_1_ET1/006sqch1.asc", save=True)
+
+gaze_fixation = calculate_fixations(gaze)
+
+gaze_saccades = calculate_saccades(gaze_fixation)
+
+map_fixations_to_aois(gaze_saccades)
