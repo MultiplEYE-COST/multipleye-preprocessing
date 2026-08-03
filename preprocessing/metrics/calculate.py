aa894a7c7716fa11cf47cdc037ac12d58c1f8502
Antonia Popp <antoniamaria.popp@uzh.ch>
2026-08-03 09:55:04 +0200

    replaced calculate reading measures

--- preprocessing/metrics/calculate.py
+++ preprocessing/metrics/calculate.py
@@ -1,5 +1,6 @@
 import pymovements as pm
 import polars as pl
+from pymovements.measure.reading.processing import compute_reading_measures
 
 from preprocessing.data_collection.stimulus import Stimulus
 from preprocessing.metrics.reading.fixations import annotate_fixations
@@ -8,29 +9,39 @@ from preprocessing.metrics.reading.words import (
     mark_skipped_tokens,
     all_tokens_from_aois,
 )
+from preprocessing import settings
 
 
 def calculate_reading_measures(gaze: pm.Gaze, stimuli: list[Stimulus]) -> pl.DataFrame:
-    # create a fixation table
-    fixation_table = annotate_fixations(gaze.events.frame)
+    group_columns = [settings.TRIAL_COL, settings.STIMULUS_COL, settings.PAGE_COL]
 
-    words_only_all_trials = []
+    only_fix = (
+            gaze.events.frame.filter(
+                (pl.col("name") == settings.FIXATION)
+                & (pl.col(settings.WORD_IDX_COL).is_not_null())
+            )
+            .with_row_count("fixation_id")
+            .sort(group_columns + ["onset"])
+        )
+    rm_all_trials = []
 
     for stim in stimuli:
         aois = stim.text_stimulus.aois
         words_only = all_tokens_from_aois(aois, trial=stim.trial_id)
         words_only = words_only.with_columns(pl.lit(stim.name).alias("stimulus"))
-        words_only_all_trials.append(words_only)
+        trial_idx = stim.trial_id
 
-    words_df = pl.concat(words_only_all_trials)
+        for page in stim.pages:
+            page_idx = f"page_{page.number}"
+            page_words = words_only.filter(pl.col("page")==page_idx)
+            page_fix = only_fix.filter((pl.col("trial")==trial_idx) & (pl.col("page")==page_idx))
+            rm = compute_reading_measures(
+                fixations=page_fix,
+                aois = page_words,
+                word_index_column= "word_idx",
+                word_column = "word"
+            )
+            rm = rm.with_columns(pl.lit(trial_idx).alias("trial"), pl.lit(page_idx).alias("page"), pl.lit(stim.name).alias("stimulus"))
+            rm_all_trials.append(rm)
 
-    #  annotate skipped words based on fixation table and all tokens
-    words_with_skip = mark_skipped_tokens(words_df, fixation_table)
-
-    # calculate word-level reading measures
-    word_level_table = build_word_level_table(
-        words=words_with_skip,
-        fix=fixation_table,
-    )
-
-    return word_level_table
+    return pl.concat(rm_all_trials)
